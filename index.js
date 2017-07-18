a54624f85cc143913071d803128ed60dd023c472
Alon Niv <oakfang@gmail.com>
2017-07-18 17:42:25 +0300

    Add Resource support

--- index.js
+++ index.js
@@ -1,19 +1,23 @@
-const Module = require('module');
-const co = require('co');
-const ora = require('ora');
-const chalk = require('chalk');
-const _ = require('lodash');
+const Module = require("module");
+const co = require("co");
+const ora = require("ora");
+const chalk = require("chalk");
+const Resource = require("shared-resource");
+const _ = require("lodash");
 
 const resolveScenario = (scenario, t) => {
-  if (scenario.constructor.name === 'GeneratorFunction') {
+  if (scenario.constructor.name === "GeneratorFunction") {
     return co.wrap(scenario)(t);
   }
   return Promise.resolve(t).then(scenario);
-}
+};
 
 const failures = {};
 const errors = {};
+const before = [];
+const after = [];
 const running = new Set();
+const isTest = Symbol('@@isTest');
 
 const testInstance = name => {
   let passing = true;
@@ -35,15 +39,16 @@ const testInstance = name => {
   };
 
   const base = {
+    context: {},
     get passed() {
       return passing;
     },
     pass() {
       // do nothing
-    },
+    }
   };
 
-  base.fail = addCheck(() => false, () => 'Explicit failure');
+  base.fail = addCheck(() => false, () => "Explicit failure");
   base.is = addCheck((x, y) => x === y, (x, y) => `${x} is not ${y}`);
   base.isNot = addCheck((x, y) => x !== y, (x, y) => `${x} is ${y}`);
   base.truthy = addCheck(x => x, x => `${x} is not truthy`);
@@ -54,17 +59,17 @@ const testInstance = name => {
 
 function report() {
   if (_.isEmpty(errors) && _.isEmpty(failures)) {
-    console.log(chalk.green('All tests passed successfully'));
+    console.log(chalk.green("All tests passed successfully"));
     return;
   }
   if (!_.isEmpty(failures)) {
-    console.error(chalk.red('===========FAILURES============='));
-    _.forEach(failures, (msgs, name) => msgs.forEach(msg =>
-      console.error(chalk.red(`${name}: ${msg}`))
-    ));
+    console.error(chalk.red("===========FAILURES============="));
+    _.forEach(failures, (msgs, name) =>
+      msgs.forEach(msg => console.error(chalk.red(`${name}: ${msg}`)))
+    );
   }
   if (!_.isEmpty(errors)) {
-    console.error(chalk.red('============ERRORS=============='));
+    console.error(chalk.red("============ERRORS=============="));
     _.forEach(errors, (err, name) => {
       console.error(chalk.red(name));
       console.error(chalk.red(err));
@@ -73,27 +78,30 @@ function report() {
   process.exit(1);
 }
 
-function test(testName, scenario) {
+const test = Resource.wrap(async (testName, scenario) => {
   const spinner = ora(testName).start();
   const t = testInstance(testName);
-  running.add(t);
-  return resolveScenario(scenario, t)
-    .then(() => {
-      if (t.passed) {
-        spinner.succeed();
-      } else {
-        spinner.fail();
-      }
-    }).catch(e => {
-      errors[testName] = e;
+  Resource.now[isTest] = true;
+  Resource.now.context = t.context;
+  try {
+    await Promise.resolve(before.map(fn => Promise.resolve(t).then(fn)));
+    running.add(t);
+    await resolveScenario(scenario, t);
+    if (t.passed) {
+      spinner.succeed();
+    } else {
       spinner.fail();
-    }).then(() => {
-      running.delete(t);
-      if (!running.size) report();
-    });
-}
+    }
+    await Promise.resolve(after.map(fn => Promise.resolve(t).then(fn)));
+  } catch (e) {
+    errors[testName] = e;
+    spinner.fail();
+  }
+  running.delete(t);
+  if (!running.size) report();
+});
 
-test.patch = (patches) => {
+test.patch = patches => {
   const moduleCache = Module._cache;
   const resolved = Object.keys(patches).reduce((ms, m) => {
     ms[require.resolve(m)] = patches[m];
@@ -103,11 +111,15 @@ test.patch = (patches) => {
     get(cache, resolvedModule) {
       const patch = resolved[resolvedModule];
       return patch ? { exports: patch } : cache[resolvedModule];
-    },
+    }
   });
   return () => {
     Module._cache = moduleCache;
   };
 };
+
+test.beforeEach = fn => before.push(fn);
+test.afterEach = fn => after.push(fn);
+test.isTest = isTest;
 
 module.exports = test;
--- test.js
+++ test.js
@@ -1,4 +1,16 @@
+const Resource = require('shared-resource');
 const test = require('.');
+
+const sleep = t => new Promise(resolve => setTimeout(resolve, t));
+
+async function foo() {
+  await sleep(3000);
+  return Resource.now[test.isTest] ? Resource.now.context.a : 1;
+}
+
+test.beforeEach(t => {
+  t.context.a = 5;
+});
 
 test('A sync test', t => {
   t.is(2, 2);
@@ -7,6 +19,10 @@ test('A sync test', t => {
 
 test('Async test', function* (t) {
   t.is((yield Promise.resolve(2)), 2);
+});
+
+test('Awesome aware async test', async t => {
+  t.is(await foo(), 5);
 });
 
 test('Failing test', t => t.fail('with a message'));
